e50010c894240ff7adb3bf080bbede7da13db346
KubaK1P <1p22kus@gmail.com>
2025-09-06 13:51:56 +0200

    Update task section styles and heading in Main component
    Refactored the 'Moje zadania' heading to use a new 'myTasks' class for improved styling and removed the separate <hr /> element. Enhanced the main heading's appearance and adjusted layout styles in App.css for a more polished UI.

--- to-do-app/src/components/Main.js
+++ to-do-app/src/components/Main.js
@@ -7,8 +7,7 @@ function Main() {
         <div className="main">
             <main >
                 <h2 className="mainHeading">Do zrobienia</h2>
-                <h3>Moje zadania</h3>
-                <hr />
+                <h3 className="myTasks">Moje zadania</h3>
                 <div>
                     <ToDoList></ToDoList>
                 </div>
